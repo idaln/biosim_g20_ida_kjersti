d459b0f9b30ab1c164ee42d3078051fce78b22b3
idaln <idna@nmbu.no>
2020-01-08 11:04:33 +0100

    Fixed PEP8

--- tests/test_animals.py
+++ tests/test_animals.py
@@ -4,17 +4,21 @@
 __author__ = "Ida Lunde Naalsund & Kjersti Rustad Kvisberg"
 __email__ = "idaln@hotmail.com & kjkv@nmbu.no"
 
+
 def test_ageing():
     """ Checks that animal is one year older than last year."""
     pass
+
 
 def test_weight_loss():
     """ Checks that weight after weight loss is less than initial weight"""
     pass
 
+
 def test_eat():
     """ Checks that animal has gained weight after eating."""
     pass
+
 
 def test_fitness():
     """ Checks that fitness is between 0 and 1."""
