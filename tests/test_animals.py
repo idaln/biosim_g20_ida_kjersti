295b0adde74ea6718a3de839b91acd3b5fd961fa
idaln <idna@nmbu.no>
2020-01-08 10:43:21 +0100

    Created function shell for test of weight loss method in Animal

--- tests/test_animals.py
+++ tests/test_animals.py
@@ -3,3 +3,7 @@
 
 __author__ = "Ida Lunde Naalsund & Kjersti Rustad Kvisberg"
 __email__ = "idaln@hotmail.com & kjkv@nmbu.no"
+
+def test_weight_loss():
+    """ Checks that weight after weight loss is less than initial weight"""
+    pass
