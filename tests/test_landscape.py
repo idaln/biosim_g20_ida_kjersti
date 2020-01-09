46856103a63aff477424dc2359e9040dcd1e58f9
Kjersti <kjkv@nmbu.no>
2020-01-09 14:13:11 +0100

    Added test classes for Landscape and Jungle

--- tests/test_landscape.py
+++ tests/test_landscape.py
@@ -2,3 +2,20 @@
 
 __author__ = "Ida Lunde Naalsund & Kjersti Rustad Kvisberg"
 __email__ = "idaln@hotmail.com & kjkv@nmbu.no"
+
+class TestLandscape:
+    """
+    Test class for Landscape class.
+    """
+    def test_constructor(self):
+
+    def test_fodder_loss(self):
+        """
+        Asserts that amount of fodder has decreased after grazing by
+        an animal.
+        """
+
+class TestJungle:
+    """
+    Tests for Jungle class.
+    """
