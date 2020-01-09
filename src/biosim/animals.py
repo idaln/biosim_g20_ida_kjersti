a0050b100a6dc2216e41940e2893f3209e51528d
idaln <idna@nmbu.no>
2020-01-09 13:08:56 +0100

    Had to move parameters out of init function.
    In order to run tests, we had to move our test parameters into the Animal class.

--- src/biosim/animals.py
+++ src/biosim/animals.py
@@ -49,27 +49,29 @@ class Animal:
     """
     Parent class for herbivores and carnivores
     """
+    params = {
+        "w_birth": 6.0,
+        "sigma_birth": 1.0,
+        "beta": 0.75,
+        "eta": 0.125,
+        "a_half": 60.0,
+        "phi_age": 0.4,
+        "w_half": 4.0,
+        "phi_weight": 0.4,
+        "mu": 0.4,
+        "lambda": 1.0,
+        "gamma": 0.8,
+        "zeta": 3.5,
+        "xi": 1.1,
+        "omega": 0.9,
+        "F": 50.0,
+        "DeltaPhiMax": 10.0
+    }
 
-    def __init__(self, params, properties, num_animals):
+    def __init__(self, properties):
         """
         Initializing class by unpacking all parameters given as input.
         """
-        self.w_birth = params["w_birth"]
-        self.sigma_birth = params["sigma_birth"]
-        self.beta = params["beta"]
-        self.eta = params["eta"]
-        self.a_half = params["a_half"]
-        self.phi_age = params["phi_age"]
-        self.w_half = params["w_half"]
-        self.phi_weight = params["phi_weight"]
-        self.mu = params["mu"]
-        self._lambda = params["lambda"]
-        self.gamma = params["gamma"]
-        self.zeta = params["zeta"]
-        self.xi = params["xi"]
-        self.omega = params["omega"]
-        self.F = params["F"]
-        self.DeltaPhiMax = params["DeltaPhiMax"]
 
         self.weight = properties["weight"]
         self.age = properties["age"]
@@ -79,7 +81,6 @@ class Animal:
         else:
             self.fitness = properties["fitness"]
 
-        self.num_animals = num_animals
 
     def ageing(self):
         """
@@ -92,7 +93,7 @@ class Animal:
         Substracts given amount of weight from the animals
         total weight after each cycle, given by eta*weight
         """
-        new_weight = (1 - self.eta) * self.weight
+        new_weight = (1 - self.params['eta']) * self.weight
         self.weight = new_weight
 
     def eat(self, fodder):
@@ -102,15 +103,15 @@ class Animal:
         :param fodder
                Amount of fodder available to the animal
         """
-        self.weight += self.beta * fodder
+        self.weight += self.params['beta'] * fodder
 
     def find_fitness(self):
         """
         Updates fitness.
         Fitness is zero if weight is zero, otherwise given by formula (3).
         """
-        q_plus = 1/(1 + np.exp(self.phi_age*(self.age - self.a_half)))
-        q_minus = 1/(1 + np.exp(-self.phi_weight*(self.weight - self.w_half)))
+        q_plus = 1/(1 + np.exp(self.params["phi_age"]*(self.age - self.params["a_half"])))
+        q_minus = 1/(1 + np.exp(-self.params["phi_weight"]*(self.weight - self.params["w_half"])))
 
         if self.weight <= 0:
             self.fitness = 0
@@ -124,26 +125,26 @@ class Animal:
         """
         pass
 
-    @property
-    def prob_give_birth(self):
+
+    def prob_give_birth(self, num_animals):
         """
         Checks that weight of animal is more than given limit. If so,
         probability of giving birth is calculated from formula (8).
         :returns prob
                  Probability of giving birth
         """
-        if self.weight < self.zeta * (self.w_birth + self.sigma_birth):
+        if self.weight < self.params['zeta'] * (self.params['w_birth'] + self.params['sigma_birth']):
             return 0
         else:
-            return min(1, self.gamma * self.fitness * (self.num_animals - 1))
+            return min(1, self.params['gamma'] * self.fitness * (num_animals - 1))
 
-    def bool_give_birth(self):
+    def bool_give_birth(self, num_animals):
         """
         Checks probability of giving birth and finds out if a baby is to be
         born.
         :returns bool
         """
-        prob = self.prob_give_birth
+        prob = self.prob_give_birth(num_animals)
         random_number = np.random.random()
 
         if random_number <= prob:
@@ -151,7 +152,7 @@ class Animal:
         else:
             return False
 
-    def birth_process(self):
+    def birth_process(self, num_animals):
         """
         If birth takes place, a birth weight is returned. If not, None is
         returned. Weight of mother is reduced according to gived formula.
@@ -159,13 +160,13 @@ class Animal:
         :returns birth_weight or None
                  int, float
         """
-        bool_birth = self.bool_give_birth()
+        bool_birth = self.bool_give_birth(num_animals)
         if bool_birth is True:
-            birth_weight = np.random.normal(self.w_birth, self.sigma_birth)
+            birth_weight = np.random.normal(self.params['w_birth'], self.params['sigma_birth'])
             if birth_weight == 0:
                 return None
             else:
-                self.weight -= birth_weight * self.xi
+                self.weight -= birth_weight * self.params['xi']
                 return birth_weight
         else:
             return None
@@ -178,7 +179,7 @@ class Animal:
         if self.fitness == 0:
             return 1
         else:
-            return self.omega * (1 - self.fitness)
+            return self.params['omega'] * (1 - self.fitness)
 
     def bool_death(self):
         """
@@ -186,6 +187,6 @@ class Animal:
         and False if not.
         :return bool
         """
-        pass
+        
 
 
