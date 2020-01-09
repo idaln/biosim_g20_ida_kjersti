31927fbcbbad9b2066f8126339cac57d87dbf2a2
Kjersti <kjkv@nmbu.no>
2020-01-09 14:25:45 +0100

    Removed Landscape class

--- src/biosim/landscape.py
+++ src/biosim/landscape.py
@@ -4,10 +4,22 @@ __author__ = "Ida Lunde Naalsund & Kjersti Rustad Kvisberg"
 __email__ = "idaln@hotmail.com & kjkv@nmbu.no"
 
 
-class Landscape:
+class Jungle:
     """
-    Parent class for all landscape types.
+    Class for Jungle landscape type.
     """
-    params = {}
+    params = {
+        "f_max": 800,
+    }
 
-    def __init__(self):
+    def __init__(self, fodder_amount):
+        """
+        Initializes class with given properties.
+        """
+        self.fodder_amount = fodder_amount
+
+    def regrowth(self):
+        """
+        Sets amount of fodder to maximum after each year.
+        :return:
+        """
