1fbdbbe92238de61d8b3233ec2bd6098d7e162a4
Kjersti <kjkv@nmbu.no>
2020-01-09 14:02:08 +0100

    Added landscape file and added shell and init for Landscape class

---
+++ src/biosim/landscape.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "Ida Lunde Naalsund & Kjersti Rustad Kvisberg"
+__email__ = "idaln@hotmail.com & kjkv@nmbu.no"
+
+
+class Landscape:
+    """
+    Parent class for all landscape types.
+    """
+    params = {}
+
+    def __init__(self):
