85508ae6f7e6f007c09071fd497dcfa44c8336f5
Kjersti <kjkv@nmbu.no>
2020-01-10 11:31:49 +0100

    Started parent class for all landscape types

--- src/biosim/landscape.py
+++ src/biosim/landscape.py
@@ -6,7 +6,44 @@ __email__ = "idaln@hotmail.com & kjkv@nmbu.no"
 from biosim.animals import Animal, Herbivore
 
 
-class Jungle:
+class Landscape:
+    """
+    Parent class for all landscape types.
+    """
+    params = {
+        "f_max": 800,
+        "alpha": None
+    }
+    pop_carn = []
+    pop_herb = []
+
+    def __init__(self, population):
+        """
+        Initializes class with given population.
+        :param population: list of dictionaries
+        """
+        self.fodder_amount = None
+        self.num_carn = len(self.pop_carn)
+        self.num_herb = len(self.pop_herb)
+
+    def available_fodder_herb(self, herbivore):
+        """
+        Returns amount of fodder available to the herbivore.
+        :return available_fodder: float
+        """
+        desired_fodder = herbivore.params["F"]
+        old_fodder = self.fodder_amount
+        if self.fodder_amount >= desired_fodder:
+            self.fodder_amount -= desired_fodder
+            return desired_fodder
+        elif 0 < self.fodder_amount < desired_fodder:
+            self.fodder_amount = 0
+            return old_fodder
+        else:
+            return 0
+
+
+class Jungle(Landscape):
     """
     Class for Jungle landscape type.
     """
@@ -16,10 +53,11 @@ class Jungle:
     pop_carn = []
     pop_herb = []
 
-    def __init__(self):
+    def __init__(self, population):
         """
         Initializes class.
         """
+        super().__init__(population)
         self.fodder_amount = None
         self.num_carn = len(self.pop_carn)
         self.num_herb = len(self.pop_herb)
