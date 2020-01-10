e8ad6ceb2a5dd490dd57b873ffc18e2709e86145
idaln <idna@nmbu.no>
2020-01-10 10:55:32 +0100

    Created file for Annual Cycle

--- src/biosim/annual_cycle.py
+++ src/biosim/annual_cycle.py
@@ -3,3 +3,10 @@
 __author__ = "Ida Lunde Naalsund & Kjersti Rustad Kvisberg"
 __email__ = "idna@nmbu.no & kjkv@nmbu.no"
 
+from biosim.animals import Animal, Herbivore
+from biosim.landscape import Jungle
+
+class Annual_Cycle:
+    """
+    Class that
+    """
