931cfe58ca7e12a3b1870ab9ae027a77b59003d0
Kjersti <kjkv@nmbu.no>
2020-01-11 13:08:28 +0100

    Added IslandMap class that will contain the island

---
+++ src/biosim/island_map.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "Ida Lunde Naalsund & Kjersti Rustad Kvisberg"
+__email__ = "idna@nmbu.no & kjkv@nmbu.no"
+
+from biosim.animals import Herbivore
+from biosim.landscape import Jungle
+
+
+class IslandMap:
+    """
+    Class that contains all island cells.
+    """
+    def __init__(self):
+        """
+        Initialize map class with given 
+        """
