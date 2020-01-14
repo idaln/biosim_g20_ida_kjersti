741d85eb4d4abd4d58e50701fea3fc48985f0119
Kjersti <kjkv@nmbu.no>
2020-01-14 11:33:26 +0100

    Added code for move_single_animal and tested code in main block

--- src/biosim/island_map.py
+++ src/biosim/island_map.py
@@ -4,6 +4,7 @@ __author__ = "Ida Lunde Naalsund & Kjersti Rustad Kvisberg"
 __email__ = "idna@nmbu.no & kjkv@nmbu.no"
 
 from biosim.landscape import Jungle
+from biosim.landscape import Animal
 import textwrap
 
 
@@ -79,10 +80,12 @@ class IslandMap:
 
     def neighbours_of_current_cell(self, current_coordinates):
         """
-        Finds the neighbours of a given cell, only those an animal can move to
-        are returned.
+        Finds all neighbours of a given cell, and those an animal can
+        move to are returned.
         :param current_coordinates: tuple
-        :returns: dict of coordinate-class instance pairs
+                Location of current cell
+        :returns: dict
+                Has locations as keys and lanscape class instance as values.
         """
         neighbours_of_current_cell = {}
         n, m = current_coordinates[0], current_coordinates[1]
@@ -94,12 +97,22 @@ class IslandMap:
                     neighbours_of_current_cell[neighbour] = self.map[neighbour]
         return neighbours_of_current_cell
 
-    def move_single_animal(self, current_coordinates):
+    def move_single_animal(self, current_coordinates, single_animal):
         """
         Moves an animal to the chosen neighbouring cell. New and old cell get
         updated population lists.
         :param current_coordinates: tuple
+        :param single_animal: class instance of animal class
         """
+        neighbours_of_current_cel = self.neighbours_of_current_cell(
+            current_coordinates
+        )
+        new_coordinates = single_animal.return_new_coordinates(
+            neighbours_of_current_cel
+        )
+        if new_coordinates is not None:
+            self.map[new_coordinates].pop_herb.append(single_animal)
+            self.map[current_coordinates].pop_herb.remove(single_animal)
 
     def move_all_animals_in_cell(self, current_coordinates):
         """
@@ -152,36 +165,35 @@ class IslandMap:
 
 if __name__ == "__main__":
     test_geogr = """\
-                    JJJ
-                    JJJ
-                    JJJ
+                    J
+                    J
                     """
-
     test_ini_pop = [
         {
-            "loc": (1, 2),
+            "loc": (1, 1),
             "pop": [
                 {"species": "Herbivore", "age": 5, "weight": 20}
-                for _ in range(8)
+                for _ in range(1)
             ]
         },
         {
-            "loc": (2, 2),
+            "loc": (2, 1),
             "pop": [
                 {"species": "Herbivore", "age": 5, "weight": 20}
-                for _ in range(8)
+                for _ in range(1)
             ]
         }
 
     ]
-
-
+    test_properties = {
+        "species": "animal",
+        "age": 5,
+        "weight": 20
+    }
     i_m = IslandMap(test_geogr, test_ini_pop)
-    i_m.create_geography_dict()
     i_m.create_map_dict()
-    print(i_m.geography)
-
-#    for year in range(10):
-#        i_m.run_all_seasons()
-
+    i_m.feeding_season()
+    print(f"(1, 1):{i_m.map[(1, 1)].pop_herb}, (2, 1): {i_m.map[(2, 1)].pop_herb}")
+    i_m.move_single_animal((2, 1), i_m.map[(2, 1)].pop_herb[0])
+    print(f"(1, 1):{i_m.map[(1, 1)].pop_herb}, (2, 1): {i_m.map[(2, 1)].pop_herb}")
 
