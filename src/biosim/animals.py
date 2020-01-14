664bee508e494e377b0b641d53babac54611626e
idaln <idna@nmbu.no>
2020-01-14 13:54:59 +0100

    Added fitness update to method finding prob ob moving

--- src/biosim/animals.py
+++ src/biosim/animals.py
@@ -108,7 +108,6 @@ class Animal:
         :return: float
                 Probability of moving
         """
-        self.find_fitness()
         return self.fitness * self.params["mu"]
 
     def will_animal_move(self):
