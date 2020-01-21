7fd3aaa7099b64baef6f2637efdee78dee8247cd
Kjersti <kjkv@nmbu.no>
2020-01-21 18:42:41 +0100

    More changes to docstrings, variable names and method names

--- tests/test_island_map.py
+++ tests/test_island_map.py
@@ -57,8 +57,8 @@ class TestIslandMap:
             self, example_ini_pop
     ):
         """
-        Asserts that the create_geography method creates a dictionary that is
-        converted correctly.
+        Asserts that the create_geography method creates a dictionary, and
+        that the dictionary has correct keys and values.
         """
         geogr_convert = """\
                             OOOO
@@ -80,8 +80,9 @@ class TestIslandMap:
             self, example_geogr, example_ini_pop
     ):
         """
-        Asserts that the create_population method created a dictionary that is
-        converted correctly.
+        Asserts that the create_population method created a dictionary and
+        that the keys and values of the dictionary have been converted
+        correctly.
         """
         island_map = IslandMap(example_geogr, example_ini_pop)
         island_map.create_population_dict()
@@ -99,20 +100,18 @@ class TestIslandMap:
             ]
         }
 
-    def test_map_is_dict(self, example_geogr, example_ini_pop):
+    def test_map_of_island_is_dict(self, example_geogr, example_ini_pop):
         """
-        Asserts that the create_landscape_cells_with_population created a
-        dictionary
+        Asserts that the create_map_dict method created a dictionary.
         """
         island_map = IslandMap(example_geogr, example_ini_pop)
         island_map.create_map_dict()
         assert type(island_map.map) is dict
 
-    def test_map_includes_all_landscape_types(self, example_ini_pop):
+    def test_map_can_include_all_landscape_types(self, example_ini_pop):
         """
-        Asserts that map can be created with all landscape types.
-        :param example_ini_pop: list
-                Initial population
+        Asserts that create_map_dict can create map from a geography string
+        where all landscape types are included.
         """
         all_types = """\
                         OOOO
@@ -128,13 +127,22 @@ class TestIslandMap:
         assert type(island_map.map[(2, 1)]).__name__ is "Savannah"
         assert type(island_map.map[(2, 2)]).__name__ is "Jungle"
 
-    def test_add_animals(self, example_ini_pop, example_geogr):
+    def test_add_animals_to_existing_population(
+            self, example_ini_pop, example_geogr
+    ):
+        """
+        Tests add_population method.
+        Asserts that additional animals can be added to an existing population.
+        """
         island_map = IslandMap(example_geogr, initial_population=[])
         island_map.create_map_dict()
         island_map.add_population(example_ini_pop)
 
-    def test_feeding_season(self, example_geogr, example_ini_pop):
+    def test_one_animal_in_each_cell_has_been_fed(
+            self, example_geogr, example_ini_pop
+    ):
         """
+        Tests feeding_season method.
         Asserts that one animal in each cell of the test island
         have been fed.
         """
@@ -146,17 +154,13 @@ class TestIslandMap:
         assert island_map.map[(2, 2)].pop_herb[0].weight > \
             example_ini_pop[1]["pop"][0]["weight"]
 
-    def test_all_animals_gave_birth(
+    def test_all_animals_gave_birth_when_prob_birth_is_one(
             self, mocker, example_ini_pop, example_geogr
     ):
         """
+        Tests procreation_season method.
         Asserts that all animals on island give birth when their probability
         of giving birth is one.
-        :param example_ini_pop: list
-                Initial population
-        :param example_geogr: str
-                Map
-        :return:
         """
         mocker.patch("numpy.random.random", return_value=0.01)
         island_map = IslandMap(example_geogr, example_ini_pop)
@@ -170,13 +174,14 @@ class TestIslandMap:
             sum_animals += len(cell.pop_herb)
             sum_animals += len(cell.pop_carn)
         assert sum_animals == 2 * ini_sum_animals
+        # length of population has doubled when all animals have given birth
 
-    def test_zero_neighbours(self, example_ini_pop):
+    def test_finds_no_neighbours_when_no_habitable_cells_around(
+            self, example_ini_pop
+    ):
         """
         Asserts that the neighbours_of_current_cell method handles a cell
         with no habitable neighbours.
-        :param example_ini_pop: list of dicts
-                Initial population of map
         """
         geogr_one_cell = """\
                             OOO
@@ -188,23 +193,26 @@ class TestIslandMap:
         dict_with_neighbours = island_map.neighbours_of_current_cell((1, 1))
         assert dict_with_neighbours == {}
 
-    def test_two_correct_neighbours(self, example_geogr, example_ini_pop):
+    def test_finds_two_correct_neighbours_when_two_habitable_cells_around(
+            self, example_geogr, example_ini_pop
+    ):
         """
         Asserts that the neighbours_of_current_cell method return the
         correct neighbours for a cell with only two neighbours.
         """
         island_map = IslandMap(example_geogr, example_ini_pop)
         island_map.create_map_dict()
-
         dict_with_neighbours = island_map.neighbours_of_current_cell((1, 1))
         neighbours = [(1, 2), (2, 1)]
         for neighbour in dict_with_neighbours.keys():
             assert neighbour in neighbours
 
-    def test_four_correct_neighbours(self, example_ini_pop):
+    def test_finds_four_correct_neighbours_when_four_habitable_cells_around(
+            self, example_ini_pop
+    ):
         """
         Asserts that the neighbours_of_current_cell method return the
-        correct neighbours of a cell with four neighbours.
+        correct neighbours for a cell with four neighbours.
         """
         test_geogr = """\
                             OOOOO
@@ -215,16 +223,18 @@ class TestIslandMap:
                             """
         island_map = IslandMap(test_geogr, example_ini_pop)
         island_map.create_map_dict()
-
         dict_with_neighbours = island_map.neighbours_of_current_cell((2, 2))
         neighbours = [(1, 2), (2, 1), (2, 3), (3, 2)]
         for neighbour in dict_with_neighbours.keys():
             assert neighbour in neighbours
 
-    def test_animal_moves(self, mocker, example_geogr):
+    def test_all_animals_move_if_rand_num_less_than_prob(
+            self, mocker, example_geogr
+    ):
         """
-        Asserts that all animal moves if they have not moved this year and
-        mocking random number makes sure they will.
+        Tests move_single_animal on several animals.
+        Asserts that all animal move if they have not moved this year and
+        random number is less than their probability to move.
         """
         move_ini_pop = [
             {
@@ -251,10 +261,11 @@ class TestIslandMap:
             for carnivore in cell.pop_carn:
                 assert island_map.move_single_animal(loc, carnivore) is True
 
-    def test_animal_has_already_moved(
-            self, mocker, example_geogr, example_ini_pop
+    def test_animal_moves_not_if_it_has_already_moved_this_year(
+            self, example_geogr, example_ini_pop
     ):
         """
+        Tests move_single_animal.
         Asserts that animal does not move if it has already moved this year.
         """
         island_map = IslandMap(example_geogr, example_ini_pop)
@@ -263,10 +274,13 @@ class TestIslandMap:
         herbivore.has_moved_this_year = True
         assert island_map.move_single_animal((1, 2), herbivore) is None
 
-    def test_animal_moves_not(self, mocker, example_geogr, example_ini_pop):
+    def test_single_animal_moves_not_if_rand_num_higher_than_prob(
+            self, mocker, example_geogr, example_ini_pop
+    ):
         """
+        Tests move_single_animal.
         Asserts that animal does not move if it has not moved this year, but
-        mocking the random number makes sure it will not.
+        the random number is higher than the probability of moving.
         """
         mocker.patch('numpy.random.random', return_value=1)
         island_map = IslandMap(example_geogr, example_ini_pop)
@@ -274,21 +288,26 @@ class TestIslandMap:
         herbivore = island_map.map[(1, 2)].pop_herb[0]
         assert island_map.move_single_animal((1, 2), herbivore) is None
 
-    def test_all_animals_move(self, mocker, example_ini_pop, example_geogr):
+    def test_all_animals_move_when_rand_num_less_than_prob(
+            self, mocker, example_ini_pop, example_geogr
+    ):
         """
-        Asserts that all animals move out of a cell when mocking the random
-        number makes sure they will.
+        Tests move_all_animals_in_cell.
+        Asserts that all animals move out of a cell when the random number is
+        less than the probability for each to move.
         """
         mocker.patch('numpy.random.random', return_value=0.0001)
         island_map = IslandMap(example_geogr, [example_ini_pop[0]])
         island_map.create_map_dict()
         island_map.move_all_animals_in_cell((1, 2), island_map.map[(1, 2)])
         final_num_animals = len(island_map.map[(1, 2)].pop_herb) + \
-                            len(island_map.map[(1, 2)].pop_carn)
+            len(island_map.map[(1, 2)].pop_carn)
         assert final_num_animals == 0
+        # length of population is zero when all animals have moved
 
     def test_no_animals_move(self, mocker, example_ini_pop, example_geogr):
         """
+        Tests move_all_animals_in_cell.
         Asserts that no animals move out of a cell when mocking the random
         number makes sure they will not.
         """
