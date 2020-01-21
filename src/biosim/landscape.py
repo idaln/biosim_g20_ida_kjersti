2b27d43667e4da99cc135a45728d1ade945df132
Kjersti <kjkv@nmbu.no>
2020-01-21 11:37:22 +0100

    Rewrote some documentation

--- src/biosim/landscape.py
+++ src/biosim/landscape.py
@@ -26,36 +26,49 @@ class Landscape:
 
     @classmethod
     def GET_DEFAULT_PARAMS(cls):
+        """
+        Returns a copy of the default landscape parameters.
+
+        :return: Copy of default landscape parameters
+        :rtype: dict
+        """
         return cls._DEFAULT_PARAMS.copy()
 
     @classmethod
     def reset_params(cls):
+        """
+        Sets the landscape parameters equal to default.
+        """
         cls.params = cls.GET_DEFAULT_PARAMS()
 
     def __init__(self, population):
-        """Initializes class with given population.
-        :param population: A list containing dictionaries containing
-        information about each animal
+        """
+        Initializes class with given population. Creates instances of
+        correct species for all elements in population list, and adds the
+        instances to correct population list.
+
+        :param population: Contains dictionaries containing
+            information about each animal
         :type population: list
         """
         self.fodder_amount = 0
         self.pop_carn = []
         self.pop_herb = []
         
-        for individual in population:
-            if individual["species"] is "Herbivore":
-                self.pop_herb.append(Herbivore(individual))
+        for animal_info in population:
+            if animal_info["species"] is "Herbivore":
+                self.pop_herb.append(Herbivore(animal_info))
             else:
-                self.pop_carn.append(Carnivore(individual))
+                self.pop_carn.append(Carnivore(animal_info))
 
     def sort_herb_population_by_fitness(self):
         """
-        Sorts herbivore populations by fitness, from highest to
+        Sorts herbivore population by fitness, from highest to
         lowest. Uses lambda sorting.
         """
-        for individual in self.pop_herb:
-            if individual.fitness_must_be_updated is True:
-                individual.find_fitness()
+        for herb in self.pop_herb:
+            if herb.fitness_must_be_updated is True:
+                herb.find_fitness()
         self.pop_herb = sorted(self.pop_herb, key=lambda x: x.fitness,
                                reverse=True)
 
@@ -64,32 +77,35 @@ class Landscape:
         Sorts carnivore population by fitness, from highest to
         lowest. Uses lambda sorting.
         """
-        for individual in self.pop_carn:
-            if individual.fitness_must_be_updated is True:
-                individual.find_fitness()
+        for carn in self.pop_carn:
+            if carn.fitness_must_be_updated is True:
+                carn.find_fitness()
         self.pop_carn = sorted(self.pop_carn, key=lambda x: x.fitness,
                                reverse=True)
 
     def regrowth(self):
         """
-        Sets amount of fodder for herbivores to maximum at the beginning of
-        each year.
+        Sets amount of fodder for herbivores to maximum.
         """
         self.fodder_amount = self.params["f_max"]
 
     def available_fodder_herbivore(self):
         """
-        Returns amount of fodder available to the herbivore.
-        :return available_fodder: float
+        Returns amount of fodder available to an herbivore. If plenty of fodder
+        is available in the cell, enough fodder will be returned to fullfill
+        it's appetite F. If not, what's left will be returned.
+
+        :return: Amount of fodder available to an herbivore.
+        :rtype: float
         """
-        desired_fodder = Herbivore.params["F"]
-        old_fodder = self.fodder_amount
-        if self.fodder_amount >= desired_fodder:
-            self.fodder_amount -= desired_fodder
-            return desired_fodder
-        elif 0 < self.fodder_amount < desired_fodder:
+        desired_fodder_amount = Herbivore.params["F"]
+        previous_fodder_amount = self.fodder_amount
+        if self.fodder_amount >= desired_fodder_amount:
+            self.fodder_amount -= desired_fodder_amount
+            return desired_fodder_amount
+        elif 0 < self.fodder_amount < desired_fodder_amount:
             self.fodder_amount = 0
-            return old_fodder
+            return previous_fodder_amount
         else:
             return 0
 
@@ -97,11 +113,13 @@ class Landscape:
         """
         Returns amount of fodder available to carnivore. That is, the total
         weight of the herbivores in the cell.
+        :return: Amount of fodder available to a carnivore.
+        :rtype: float
         """
-        available_fodder = 0
+        available_fodder_amount = 0
         for herb in self.pop_herb:
-            available_fodder += herb.weight
-        return available_fodder
+            available_fodder_amount += herb.weight
+        return available_fodder_amount
 
     def feed_all_herbivores(self):
         """
@@ -113,19 +131,10 @@ class Landscape:
         for herb in self.pop_herb:
             herb.add_eaten_fodder_to_weight(self.available_fodder_herbivore())
 
-    def remove_all_eaten_herbivores(self, eaten_herbivores):
-        """
-        Removed herbivores that have been eaten from pop_herb
-        :param eaten_herbivores: list
-                List of herbivores that have been eaten
-        """
-        self.pop_herb = [herb for herb in self.pop_herb
-                         if herb not in eaten_herbivores]
-
     def feed_all_carnivores(self):
         """
-        Iterates over the population of carnivores in the cell, and feeds all
-        carnivores using their inherent eating method.
+        Sorts carnivore population in the cell. Then, iterates over the
+        carnivores and feeds them all, using their inherent eating method.
         """
         self.sort_carn_population_by_fitness()
         for carn in self.pop_carn:
@@ -133,31 +142,45 @@ class Landscape:
             eaten_herbivores = carn.eat(self.pop_herb)
             self.remove_all_eaten_herbivores(eaten_herbivores)
 
+    def remove_all_eaten_herbivores(self, eaten_herbivores):
+        """
+        Removes herbivores that have been eaten from the
+        herbivore population of the cell.
+        :param eaten_herbivores: Herbivore instances that have been eaten
+            during feeding of a carnivore.
+        :type eaten_herbivores: list
+        """
+        self.pop_herb = [herb for herb in self.pop_herb
+                         if herb not in eaten_herbivores]
+
     def add_newborn_animals(self):
         """
-        Iterates over both population lists in turn,
-        and makes all animal procreate utilizing their
-        inherent birth process method.
+        Iterates over both population lists in turn, and makes all animals
+        procreate utilizing their inherent birth process method.
+        Adds a new class instance to population lists for each animal
+        that is born.
         """
-        num_animals = len(self.pop_herb)
-        for animal in self.pop_herb[:num_animals]:
-            baby_weight = animal.birth_process(num_animals)
+        initial_num_herbs = len(self.pop_herb)
+        for animal in self.pop_herb[:initial_num_herbs]:
+            baby_weight = animal.birth_process(initial_num_herbs)
             if type(baby_weight) is (float or int):
                 self.pop_herb.append(
                     Herbivore({"species": "Herbivore",
                                "age": 0,
                                "weight": baby_weight})
                 )
+                self.pop_herb[-1].fitness_must_be_updated = True
 
-        num_animals = len(self.pop_carn)
-        for animal in self.pop_carn[:num_animals]:
-            baby_weight = animal.birth_process(num_animals)
+        initial_num_carns = len(self.pop_carn)
+        for animal in self.pop_carn[:initial_num_carns]:
+            baby_weight = animal.birth_process(initial_num_carns)
             if type(baby_weight) is (float or int):
                 self.pop_carn.append(
                     Carnivore({"species": "Carnivore",
                                "age": 0,
                                "weight": baby_weight})
                 )
+                self.pop_carn[-1].fitness_must_be_updated = True
 
     def make_all_animals_older(self):
         """
@@ -178,12 +201,12 @@ class Landscape:
     def remove_all_dead_animals(self):
         """
         Iterates over population lists and runs inherent death method on all
-        animals. Updates the population to only contain living animals.
+        animals. Updates the population lists to only contain living animals.
         """
-        self.pop_herb = [animal for animal in self.pop_herb
-                         if animal.will_animal_live() is True]
-        self.pop_carn = [animal for animal in self.pop_carn
-                         if animal.will_animal_live() is True]
+        self.pop_herb = [herb for herb in self.pop_herb
+                         if herb.will_animal_live() is True]
+        self.pop_carn = [carn for carn in self.pop_carn
+                         if carn.will_animal_live() is True]
 
 
 class Jungle(Landscape):
