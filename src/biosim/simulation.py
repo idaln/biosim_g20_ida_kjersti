b6e959d531aa177b565c824e9a329306cba07297
idaln <idna@nmbu.no>
2020-01-22 09:54:22 +0100

    Made change to remove warning from sphinx

--- src/biosim/simulation.py
+++ src/biosim/simulation.py
@@ -226,7 +226,7 @@ class BioSim:
         :param num_years: number of years to simulate
         :param vis_years: years between visualization updates
         :param img_years: years between visualizations saved to files
-        (default: vis_years)
+            (default: vis_years)
 
         Image files will be numbered consecutively.
         """
